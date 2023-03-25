7b02f6bf07be666a0a7a911821d5ee00f8614d33
keilanyg <keilanydes@gmail.com>
2023-03-25 16:22:35 -0300

    Estilizações nas demais pág

--- estoque/forms.py
+++ estoque/forms.py
@@ -5,4 +5,12 @@ class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields =  "__all__"
+        widgets = {
+            "nome":forms.TextInput(attrs={"class": "form-control"}),
+            "categoria":forms.Select(attrs={"class": "form-control"}),
+            "quantidade":forms.Select(attrs={"class": "form-control"}),
+            "preco_compra":forms.TextInput(attrs={"class": "form-control"}),
+            "preco_venda":forms.TextInput(attrs={"class": "form-control"}),
+            "slug":forms.TextInput(attrs={"class": "form-control"}),
+        }
         
