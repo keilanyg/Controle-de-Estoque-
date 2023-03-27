26920442ac71a6144b3087e5d88e4be68b642ef5
keilanyg <keilanydes@gmail.com>
2023-03-26 21:20:56 -0300

    Ajustes

--- estoque/forms.py
+++ estoque/forms.py
@@ -8,7 +8,7 @@ class ProdutoForm(forms.ModelForm):
         widgets = {
             "nome":forms.TextInput(attrs={"class": "form-control"}),
             "categoria":forms.Select(attrs={"class": "form-control"}),
-            "quantidade":forms.Select(attrs={"class": "form-control"}),
+            "quantidade":forms.TextInput(attrs={"class": "form-control"}),
             "preco_compra":forms.TextInput(attrs={"class": "form-control"}),
             "preco_venda":forms.TextInput(attrs={"class": "form-control"}),
             "slug":forms.TextInput(attrs={"class": "form-control"}),
